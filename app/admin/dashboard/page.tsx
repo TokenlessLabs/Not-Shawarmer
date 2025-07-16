493d6f6c1aa0d9097eead7feafe37cc3fbc2e03a
faria <faria.rafique27@gmail.com>
2025-07-16 12:02:31 +0500

    add:admin-orders-router-frontend

---
+++ app/admin/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import OrderComponent from "../ui/dashboard/ordercomponent";
+
+export default function Dashboard(){
+    return(
+        <>
+        <OrderComponent/>
+        <OrderComponent/>
+        <OrderComponent/>
+        <OrderComponent/>
+        </>
+    )
+}
