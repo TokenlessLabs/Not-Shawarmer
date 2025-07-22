f0d929a59008db252eb25e33bf6f6ed2b976b4ed
Meerab - <munirmeerab@gmail.com>
2025-07-22 16:58:36 +0500

    fixed sidebar glitch on user side

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -4,13 +4,19 @@ import Footer from "./ui/footer";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen min-h-0 flex-col md:flex-row md:overflow-hidden">
-      <div className="w-full md:w-64">
+      
+      {/* Sidebar */}
+      <aside className="md:w-64 w-full shrink-0">
         <Sidebar />
-      </div>
-      <div className="flex-grow overflow-y-auto max-h-screen">
-        <div className="min-h-screen">{children}</div>
+      </aside>
+
+      {/* Main content */}
+      <main className="flex-1 overflow-y-auto max-h-screen">
+        <div className="min-h-screen">
+          {children}
+        </div>
         <Footer />
-      </div>
+      </main>
     </div>
   );
 }
