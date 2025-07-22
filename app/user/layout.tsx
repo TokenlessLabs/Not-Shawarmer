6da92901a937bde0db4acdb39b0b65336ccd4ef5
Meerab - <munirmeerab@gmail.com>
2025-07-22 17:18:01 +0500

    fix: highlight issue on adminside when in pastorders

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -5,12 +5,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen min-h-0 flex-col md:flex-row md:overflow-hidden">
       
-      {/* Sidebar */}
       <aside className="md:w-64 w-full shrink-0">
         <Sidebar />
       </aside>
 
-      {/* Main content */}
       <main className="flex-1 overflow-y-auto max-h-screen">
         <div className="min-h-screen">
           {children}
