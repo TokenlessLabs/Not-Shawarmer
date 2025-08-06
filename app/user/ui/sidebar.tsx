1d2a718e16031d832f24d0b06e98eb87b9279729
fastcel <munirmeerab@gmail.com>
2025-08-06 16:31:19 +0500

    skeleton for sidebar

--- app/user/ui/sidebar.tsx
+++ app/user/ui/sidebar.tsx
@@ -24,7 +24,35 @@ const Sidebar = () => {
   const { user, isLoading } = useUser();
 
   if (isLoading || !user) {
-    return <div className="p-4 text-gray-500">Loading sidebar...</div>;
+    return (
+      <aside className="h-full w-full bg-theme-light-blue text-white px-2 pt-6 pb-3 flex flex-col border-r-3 border-theme-dark-blue animate-pulse">
+        <div className="h-10 w-50 bg-theme-blue/50 rounded  my-3 mx-auto" />
+
+        <div className="border-t-3 border-theme-dark-blue mt-6 mb-6 -mx-2"></div>
+
+        {/* Fake nav links */}
+        <div className="flex flex-col gap-7 h-full">
+          {Array.from({ length: 4 }).map((_, i) => (
+            <div
+              key={i}
+              className="flex items-center gap-4 text-xl px-4 py-3 rounded-lg bg-theme-blue/70"
+            >
+              <div className="h-10 w-10 bg-theme-blue/50 rounded" />
+              <div className="h-5 w-3/4 bg-theme-blue/50 rounded" />
+            </div>
+          ))}
+        </div>
+
+        <div className="border-t-3 border-theme-dark-blue mt-4 mb-3 -mx-2"></div>
+
+        {/* Logout button skeleton */}
+        <div className="flex items-center gap-4 text-xl px-4 py-3 rounded-lg bg-red-200 mt-auto">
+          <div className="h-10 w-10 bg-red-300 rounded" />
+          <div className="h-5 w-1/2 bg-red-300 rounded" />
+        </div>
+      </aside>
+    )
+
   }
 
   const navItems = user.role === Roles.Admin ? adminLinks : userLinks;
